37390d923f03cc219a932112c4dd6552fc113c9f
pranav <pranavshirode16@gmail.com>
2025-10-23 17:04:08 +0530

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,60 @@
-package org.example;
+#include <iostream>
+using namespace std;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+class bT {
+    struct node {
+        int data;
+        node* left;
+        node* right;
+        node(int val) : data(val), left(NULL), right(NULL) {}
+    };
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+    node* root;
+
+    node* insert(node* root, int item) {
+        if (root == NULL) {
+            return new node(item);
         }
+        if (item < root->data) {
+            root->left = insert(root->left, item);
+        } else if (item > root->data) {
+            root->right = insert(root->right, item);
+        }
+        return root;
     }
+
+    public:
+    bT() : root(NULL) {}
+
+    void insert(int item) {
+        root = insert(root, item);
+    }
+
+    void inorder(node* temp) {
+        if (temp == NULL) return;
+        inorder(temp->left);
+        cout << temp->data << " ";
+        inorder(temp->right);
+    }
+
+    void display() {
+        inorder(root);
+        cout << endl;
+    }
+};
+
+int main() {
+    bT tree;
+    tree.insert(50);
+    tree.insert(30);
+    tree.insert(70);
+    tree.insert(20);
+    tree.insert(40);
+    tree.insert(60);
+    tree.insert(80);
+
+    cout << "Inorder traversal of the binary search tree:\n";
+    tree.display();
+
+    return 0;
 }
